4be8b65601edffb474abccb7cf1f4fbf0de25f37
aline <aline.rosa.cruz@gmail.com>
2022-05-24 16:47:19 -0300

    Updates

--- pages/home/home.js
+++ pages/home/home.js
@@ -3,17 +3,17 @@ export default () => {
 
     const templateHome = `
       <form class="conteudoComeço">
-      <img src="/pages/home/logo.png" alt="Logo">
+      <img src="pages/home/logo.png" alt="Logo">
       <p class="boasVindas">WELCOME TO 7 ALÉM</p>
       <p class="mensagem">Sua rede de teorias da conspiração<br>
       Feita para compartilhar histórias e visão sobre...<br>
        com muito respeito e receptividade!</p>
       <br>
       <section class="criar-entrar">
-      <a href="/#register">Criar conta</a>
+      <a href="#register">Criar conta</a>
       <br>
       <p> Já tem conta?
-      <a href="/#login">Entrar</a></p>
+      <a href="#login">Entrar</a></p>
       <section>
       </form>
     `;
