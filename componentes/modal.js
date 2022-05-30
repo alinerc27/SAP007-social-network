69f71b922514d5cb3fe996cdf9f28edb28c81468
aline <aline.rosa.cruz@gmail.com>
2022-05-30 02:45:35 -0300

    Updates

--- componentes/modal.js
+++ componentes/modal.js
@@ -1,9 +1,9 @@
-import { editPosts } from "../lib/config-firestore.js";
+import { editPosts } from '../lib/config-firestore.js';
 
 export function modalEditPost(post, containerFeed) {
-    const modalContainer = document.createElement('div');
-    const ModalEdit = `
-    <div id='modal-container' class='modal-container'>
+  const modalContainer = document.createElement('div');
+  const ModalEdit = `
+    <div id='modal-container' class='modal-container modal'>
       <p class='text-reset edit'>Edição de postagem</p>
         <div>
           <textarea name='textarea' maxlength='300' id='postEdit' class='message text-writing writing-modal'
@@ -11,43 +11,41 @@ export function modalEditPost(post, containerFeed) {
         </div>
         <span class='error-message'></span>
         <div class='save-container'>
-          <button id='save-post' class='button'>Salvar</button>
-          <button id='button-cancel' class='button-cancel button'>Cancelar</button>
+          <button id='save-post' class='btnStyle'>Salvar</button>
+          <button id='button-cancel' class='button-cancel btnStyle'>Cancelar</button>
         </div>
       </div>
       `;
-  
-    modalContainer.innerHTML = ModalEdit;
-  
-    const modal = modalContainer.querySelector('#modal-container');
-    const saveEdit = modalContainer.querySelector('#save-post');
-    const posteditado = modalContainer.querySelector('#postEdit');
-    const buttonCancel = modalContainer.querySelector('#button-cancel');
-    const msgAlert = modalContainer.querySelector('.error-message');
-    const inputPost = containerFeed.querySelector(`#postText-${post.id}`);
 
-    saveEdit.addEventListener('click', () => {
-      if (posteditado.value === '') {
-        msgAlert.classList.add('error');
-        msgAlert.innerHTML = 'Opsss! ocorreu um erro Tente novamente.';
-      } else {
-        editPosts(post.id, posteditado.value).then(() => {
-          inputPost.innerHTML = posteditado.value;
-          modalContainer.remove();
-        });
-      }
-    });
-  
-    buttonCancel.addEventListener('click', () => {
-      modalContainer.remove();
-    });
-  
-    window.addEventListener('click', (e) => {
-      if (e.target === modal) {
+  modalContainer.innerHTML = ModalEdit;
+
+  const modal = modalContainer.querySelector('#modal-container');
+  const saveEdit = modalContainer.querySelector('#save-post');
+  const posteditado = modalContainer.querySelector('#postEdit');
+  const buttonCancel = modalContainer.querySelector('#button-cancel');
+  const msgAlert = modalContainer.querySelector('.error-message');
+  const inputPost = containerFeed.querySelector(`#postText-${post.id}`);
+
+  saveEdit.addEventListener('click', () => {
+    if (posteditado.value === '') {
+      msgAlert.classList.add('error');
+      msgAlert.innerHTML = 'Opsss! ocorreu um erro Tente novamente.';
+    } else {
+      editPosts(post.id, posteditado.value).then(() => {
+        inputPost.innerHTML = posteditado.value;
         modalContainer.remove();
-      }
-    });
-    return modalContainer;
-  }
-  
- 
+      });
+    }
+  });
+
+  buttonCancel.addEventListener('click', () => {
+    modalContainer.remove();
+  });
+
+  window.addEventListener('click', (e) => {
+    if (e.target === modal) {
+      modalContainer.remove();
+    }
+  });
+  return modalContainer;
+}
--- pages/feed/feed.js
+++ pages/feed/feed.js
@@ -1,35 +1,37 @@
-import "../../lib/config-firebase.js";
-import { addPosts, getPost } from "../../lib/config-firestore.js";
-import { criarCard } from "../../componentes/card.js";
-import { auth } from "../../lib/auth-firebase.js";
+import '../../lib/config-firebase.js';
+import { addPosts, getPost } from '../../lib/config-firestore.js';
+import { criarCard } from '../../componentes/card.js';
+import { auth, userLogout } from '../../lib/auth-firebase.js';
 
 export default () => {
-  let containerFeed = document.createElement('div');
-  let templateFeed = `
-    <section class="feedcontainer">
-    <div class="postt">
-    <p class="post">Publique sua teoria &#x1F441</p>
+  const containerFeed = document.createElement('div');
+  const templateFeed = `
+    <section class='feedcontainer'>
+    <section class='headerFeed'>
+    <button id='btnLogout' type='submit'>Sair</button><br>
+    </section>
+    <div class='postt'>
+    <p class='tituloFeed'>Publique sua teoria &#x1F441</p>
     </div>
-    <input id="titulo" type="text" placeholder="Título" maxlength="90"/><br>
-
-    <input id="postText" type="text" placeholder="Sua teoria aqui" maxlength="280" /><br>
-    <span id="error-message" class="error-writepost"></span>
-    <br><button id="btnPost" type="submit">Postar</button><br>
-    <div class="sectionFeedContainer">
-    <section id="sectionNewPost" class="sectionPostClass"></section>
-    <section id="sectionPost" class="sectionPostClass"></section>
+    <input id='titulo' class='inputArea tituloArea' type='text' placeholder='Título' maxlength='90'/><br>
+    <input id='postText' class='inputArea' type='text' placeholder='Sua teoria aqui' maxlength='280' /><br>
+    <span id='error-message' class='error-writepost'></span>
+    <br><button id='btnPost' class='btnStyle tituloArea' type='submit'>Postar</button><br>
+    <div class='sectionFeedContainer'>
+    <section id='sectionNewPost' class='sectionPostClass'></section>
+    <section id='sectionPost' class='sectionPostClass'></section>
     </div></section>
     `;
 
   containerFeed.innerHTML = templateFeed;
 
-  const inputTitulo = containerFeed.querySelector("#titulo");
-  const inputPost = containerFeed.querySelector("#postText");
-  const postBtn = containerFeed.querySelector("#btnPost");
-  const sectionAllPost = containerFeed.querySelector("#sectionPost");
-  const sectionNewPost = containerFeed.querySelector("#sectionNewPost");
+  const inputTitulo = containerFeed.querySelector('#titulo');
+  const inputPost = containerFeed.querySelector('#postText');
+  const postBtn = containerFeed.querySelector('#btnPost');
+  const sectionAllPost = containerFeed.querySelector('#sectionPost');
+  const sectionNewPost = containerFeed.querySelector('#sectionNewPost');
   const msgAlert = containerFeed.querySelector('#error-message');
-
+  const logout = containerFeed.querySelector('#btnLogout');
 
   postBtn.addEventListener('click', (e) => {
     e.preventDefault();
@@ -39,17 +41,17 @@ export default () => {
       addPosts(inputTitulo.value, inputPost.value, auth.currentUser.email).then((id) => {
         let titulo = inputTitulo.value;
         let post = inputPost.value;
-        const date = new Date().toLocaleString("pt-br");
+        const date = new Date().toLocaleString('pt-br');
         const item = {
           userEmail: auth.currentUser.email,
-          "titulo":titulo,
-          "post":post,
+          'titulo': titulo,
+          'post': post,
           date,
-          likes: [],  
+          likes: [],
         };
         sectionNewPost.appendChild(criarCard(item));
-        titulo = "";
-        post = "";
+        titulo = '';
+        post = '';
       });
     }
   });
@@ -59,10 +61,16 @@ export default () => {
     arrayPosts.map(posts => {
       const elemento = criarCard(posts);
       sectionAllPost.appendChild(elemento);
-    })
+    });
   };
 
+  logout.addEventListener('click', (e) => {
+    e.preventDefault();
+    userLogout().then(() => {
+      window.location.hash = '';
+    });
+  });
 
   getPosts();
   return containerFeed;
-}
+};
